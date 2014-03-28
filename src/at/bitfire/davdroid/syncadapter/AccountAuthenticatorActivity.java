b8d378ba3db136826b87d9cebebfa6ceb5209f72
Sreekumar R <sk@iamplus.org>
2014-03-28 11:48:26 +0530

    Fix build failure
    Change-Id: I8c4f3a2d927281be159aa9d09aac69088f08dff9

--- src/at/bitfire/davdroid/syncadapter/AccountAuthenticatorActivity.java
+++ src/at/bitfire/davdroid/syncadapter/AccountAuthenticatorActivity.java
@@ -29,6 +29,7 @@ import android.accounts.AccountManager;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
