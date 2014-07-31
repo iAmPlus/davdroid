75854b6205e585793111b51eab9c71e92947419a
Sreekumar R <sk@iamplus.org>
2014-07-31 19:05:12 +0530

    Revert "Disable dictionary in webview textfields"
    This reverts commit b8561a6cf6b1e44ed25bae217ae46740c51cef60.
    
    Change-Id: I732e8f6e6454179024df9fd8b404a9f358bc3658

--- src/at/bitfire/davdroid/syncadapter/MyWebView.java
+++ src/at/bitfire/davdroid/syncadapter/MyWebView.java
@@ -3,7 +3,6 @@ package at.bitfire.davdroid.syncadapter;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.inputmethod.EditorInfo;
-import android.text.InputType;
 import android.view.inputmethod.InputConnection;
 import android.webkit.WebView;
 
@@ -23,10 +22,6 @@ public class MyWebView extends WebView {
 
 		outAttrs.imeOptions = EditorInfo.IME_FLAG_NO_ENTER_ACTION | EditorInfo.IME_FLAG_NO_EXTRACT_UI
 						| EditorInfo.IME_FLAG_NO_FULLSCREEN | (EditorInfo.IME_MASK_ACTION & EditorInfo.IME_ACTION_NEXT);
-		// Disable dictionary by using password type
-		outAttrs.inputType = InputType.TYPE_CLASS_TEXT
-                    | InputType.TYPE_TEXT_VARIATION_WEB_EDIT_TEXT
-                    | InputType.TYPE_TEXT_VARIATION_WEB_PASSWORD;
 		return connection;
 	}
 
